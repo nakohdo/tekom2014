873c219e8ef0a3eedb72f38a2d211da8e33da6ec
Frank Ralf <webmaster@nakohdo.de>
2014-11-06 18:47:05 +0100

    Add un-zipped DITA_Diet folder to repository. 	new file:   DITA_Diet

---
+++ DITA_Diet/Set_XML_CATALOG_FILES.bat
@@ -0,0 +1,14 @@
+@echo off
+REM -------------------------------------------------------------------------------------
+REM Script to set the XML_CATALOG_FILES environment variable on Windows
+REM (to be executed once from its current location (e.g. by double click)
+REM -------------------------------------------------------------------------------------
+REM Path of this file.
+set where=%~dp0
+REM Remove the trailing slash from the path.
+set where=%where:~0,-1%
+setx XML_CATALOG_FILES "%where%\DITA-OT1.8.5\catalog-dita.xml"
+REM set XML_CATALOG_FILES="%where%\DITA-OT1.8.5\catalog-dita.xml"
+echo XML_CATALOG_FILES = "%where%\DITA-OT1.8.5\catalog-dita.xml"
+echo.
+pause
---
+++ DITA_Diet/readme.txt
@@ -0,0 +1,10 @@
+===============
+ DITA Diet
+ November 2014
+===============
+This folder contains all the software and files you need to take part in this workshop.
+
+1. Save this folder anywhere on your computer.
+2. Double-click the "Set_XML_CATALOG_FILES.bat" file. This runs the batch script to set the XML catalog environment variable.
+3. Click the Notepad++shortcut to start Notepad++.
+4. Use the files in the "test" folder to check whether all is working as intended. 
